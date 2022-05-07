0557c53c3aa4c7514830be83425edf7b08c43c08
陈楼乃 <chenlounai@zzltop.com>
2022-05-07 14:21:34 +0800

    升级到 androidx

--- src/android/InstallApp.java
+++ src/android/InstallApp.java
@@ -17,10 +17,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.widget.Toast;
+
+ 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 
